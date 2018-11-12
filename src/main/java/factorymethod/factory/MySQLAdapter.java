28f3269819b0561601d671e8d027e30e722bb47a
pvasquez <vasquez.soft13@gmail.com>
2018-11-11 22:30:09 -0500

    commit release implement adapter

--- src/main/java/factorymethod/factory/MySQLAdapter.java
+++ src/main/java/factorymethod/factory/MySQLAdapter.java
@@ -6,16 +6,42 @@
 package factorymethod.factory;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import oracle.jdbc.OracleDriver;
 
 /**
  *
  * @author petervs
  */
 public class MySQLAdapter implements IDBAdapter{
+    
+    static {
+        try {
+            new com.mysql.jdbc.Driver();
+        } catch (Exception e) {
+        }
+    }
 
     @Override
     public Connection getConnection() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String urlConnection = getConnectionString();
+            String user = "root";
+            String password = "1234";
+            Connection connection = DriverManager.getConnection(urlConnection, user, password);
+            System.out.println("Connection class: "+connection.getClass().getCanonicalName());
+            return connection;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    
+    public String getConnectionString(){
+        return "jdbc:mysql://localhost:3306/patterns?zeroDateTimeBehavior=conertToNull";
     }
     
 }
